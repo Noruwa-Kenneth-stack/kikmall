55eef14193bb20bbdfec619a4ff4b0028ee50777
Noruwa-Kenneth-stack <noruwa19@gmail.com>
2025-12-29 13:04:35 +0100

    privacy

---
+++ src/app/Privacy.tsx
@@ -0,0 +1,9 @@
+// pages/privacy.tsx
+export default function Privacy() {
+  return (
+    <div>
+      <h1>Privacy Policy</h1>
+      <p>This is a placeholder privacy policy for KIKMall app.</p>
+    </div>
+  );
+}
